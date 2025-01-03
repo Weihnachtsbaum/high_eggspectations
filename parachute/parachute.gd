fed079b3d79700a0fca80e31acbe212bdfc411e0
Yarden Tenenboim <yardentenenboim@gmail.com>
2025-01-03 23:14:13 +0100

    Add parachute sfx

--- parachute/parachute.gd
+++ parachute/parachute.gd
@@ -28,4 +28,7 @@ func _on_area_entered(area: Area2D) -> void:
 			area.parachute = true
 			following = area
 			on_egg = true
+			var sfx := $"../Parachute"
+			sfx.position = position
+			sfx.play()
 			SignalBus.saved.emit()
