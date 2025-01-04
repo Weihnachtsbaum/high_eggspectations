653b01fa27e16ca693318fbd5b57283d22be4485
Yarden Tenenboim <yardentenenboim@gmail.com>
2025-01-04 10:42:30 +0100

    Add grass texture

--- world/ground.gd
+++ world/ground.gd
@@ -3,5 +3,5 @@ extends StaticBody2D
 func _physics_process(_delta: float) -> void:
 	var viewport_size: Vector2 = get_viewport().size
 	position.x = viewport_size.x / 2.
-	position.y = viewport_size.y
-	scale.x = viewport_size.x / 10.
+	position.y = viewport_size.y - 10.
+	scale.x = viewport_size.x / 1200.
